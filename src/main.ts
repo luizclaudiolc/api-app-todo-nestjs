7d498dc82db710f7bfe2b78360e9125ba0206cfe
Luiz Claudio <luiz.claudiolc@outlook.com>
2024-10-03 12:21:20 -0300

    fix: muda origin para localhost:4200

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   // Configurando o CORS
   app.enableCors({
     /* origin: 'https://app-todo-front-angular.vercel.app', */
-    origin: 'http://localhost/4200',
+    origin: 'http://localhost:4200',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders:
       'Content-Type, Accept, Authorization, token, X-Custom-Header',
